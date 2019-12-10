f4264830e4c40133644e52beb4c98322e4b7637d
Zach  Smith <zdsmith001@gmail.com>
2019-12-10 18:31:23 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,23 @@
 class SongsController < ApplicationController
+
+  get '/songs' do
+    @songs = Song.all
+    erb :'/songs/index'
+  end
+
+  get '/songs/new' do
+    erb :'/songs/new'
+  end
+
+  post "/new_song" do
+    @song = Song.create(params[:song])
+    @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
+    @song.genre = params[:genre][:name]
+  end
+
+  get "/songs/:slug" do
+    @song = Song.find_by_slug(:slug)
+    erb :'/songs/show'
+  end
+
 end
