e5e02c4a2047f3aa0d3a081def1abc24dd3b853c
Zach  Smith <zdsmith001@gmail.com>
2019-12-12 16:56:21 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,14 +1,15 @@
 class SongsController < ApplicationController
 
-  get '/songs' do
+  get "/songs" do
     @songs = Song.all
     erb :'/songs/index'
   end
 
-  get '/songs/new' do
+  get "/songs/new" do
     erb :'/songs/new'
   end
 
+<<<<<<< HEAD
   post '/songs' do
     @song = Song.create(:name => params["Name"])
     @song.artist = Artist.find_or_create_by(:name => params["Artist Name"])
@@ -16,6 +17,14 @@ class SongsController < ApplicationController
     @song.save
     erb :'songs/show'
     erb :'songs/show', locals: {message: "Successfully created song."}
+=======
+  post "/songs" do
+    @song = Song.create(params[:song])
+    @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
+    @song.genre = params[:genre][:name]
+    @song.save
+    redirect to '/songs/#{@song.slug}'
+>>>>>>> 199cdb3e22cd25fa347beeac89956c04fa52c295
   end
 
   get '/songs/:slug' do
